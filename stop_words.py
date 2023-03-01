f2e771c911c6199807f093edb65d45163da44645
Sharhad Bashar <sharhad.bashar@TS-MBP-Sharhad.targetspot.ny>
2023-03-01 13:15:36 -0500

    Added function to read and get set of stop words

---
+++ stop_words.py
@@ -0,0 +1,20 @@
+import pickle
+def read():
+    with open('raw_stop_words.txt') as file:
+        stopwords = [line.rstrip().lower() for line in file]  
+    file.close()
+    stopwords = set(stopwords)
+    return stopwords
+
+def save(stopwords_pkl):
+    with open('stop_words.pkl', 'wb') as file:
+        pickle.dump(stopwords_pkl, file, protocol = pickle.HIGHEST_PROTOCOL)
+
+def stopwords():
+    with open('stop_words.pkl', 'rb') as file:
+        stopwords = pickle.load(file)
+    file.close()
+    return stopwords
+
+save(read())
+print(len(stopwords()))

e5359df89c52757f61af36ddec0e97988c51b5c2
Sharhad Bashar <sharhad.bashar@Belal-Koura.targetspot.ny>
2023-03-09 14:03:10 -0500

    Added custom paths where stop words are saved

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ List of english stop words
 
 To get a pickle file, just run `python stopwords.py`
 
-Run that file everytime you add a new stop word to the raw_stop_words.txt
+Run that file every time you add a new stop word to the `raw_stop_words.txt`
+
+Add additional locations to `save_paths.txt` where you want to save the stop words
--- stop_words.py
+++ stop_words.py
@@ -1,4 +1,12 @@
+import os
 import pickle
+
+def get_paths():
+    with open('save_paths.txt') as file:
+        paths = [line.rstrip().lower() for line in file]  
+    file.close()
+    return paths
+
 def read():
     with open('raw_stop_words.txt') as file:
         stopwords = [line.rstrip().lower() for line in file]  
@@ -6,10 +14,16 @@ def read():
     stopwords = set(stopwords)
     return stopwords
 
-def save(stopwords_pkl):
+def save(stopwords_pkl, paths):
     with open('stop_words.pkl', 'wb') as file:
         pickle.dump(stopwords_pkl, file, protocol = pickle.HIGHEST_PROTOCOL)
-
+    for path in paths:
+        try:
+            with open(os.path.join(path, 'stop_words.pkl'), 'wb') as file:
+                pickle.dump(stopwords_pkl, file, protocol = pickle.HIGHEST_PROTOCOL)
+        except:
+            None
+    
 def stopwords():
     with open('stop_words.pkl', 'rb') as file:
         stopwords = pickle.load(file)
@@ -17,5 +31,6 @@ def stopwords():
     return stopwords
 
 if __name__ == '__main__':
-    save(read())
+    paths = get_paths()
+    save(read(), paths)
     # print(stopwords())
